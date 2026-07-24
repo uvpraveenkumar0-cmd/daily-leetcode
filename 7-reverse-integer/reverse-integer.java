29e87d8fba1e229401130ffb23f034e5a5b119b3
ulasa venkata praveen kumar <uvpraveenkumar0@gmail.com>
2026-07-24 22:30:07 +0530

    Time: 1 ms (99.97%) | Memory: 42.7 MB (45.98%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,20 @@
+class Solution
+ {
+    public int reverse(int x)
+     {
+        long rev = 0; 
+        while (x != 0)
+         {
+            int digit = x % 10;   
+            rev = rev * 10 + digit; 
+            x = x / 10;           
+         }
+        // check for overflow beyond 32-bit signed integer range
+        if (rev < Integer.MIN_VALUE || rev > Integer.MAX_VALUE)
+        {
+            return 0;
+        }
+       
+        return (int)  rev;
+    }
+}
